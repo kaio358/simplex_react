b622a0d9683d915b86d816e5f5a3e72efe52dc4e
Kaio Luiz Ferreira <53657502+kaio358@users.noreply.github.com>
2023-10-30 17:13:27 -0300

    1.2

--- src/componentes/form/Input.js
+++ src/componentes/form/Input.js
@@ -1,12 +1,16 @@
 import styles from './Input.module.css'
 function Input({type,text,name,placeholder,value,eventoTexto}){
     function resultadoInput(e){
-        eventoTexto(e.target.value)
+
+            eventoTexto(e.target)
+
     }
+
     return(
         <div className={styles.form_control} >
             <label htmlFor={name}>{text}</label>
             <input type={type} name={name} id={name} onChange={resultadoInput} placeholder={placeholder} value={value} />
+            
         </div>
     )
 }
--- src/componentes/layout/Caixa.js
+++ src/componentes/layout/Caixa.js
@@ -11,13 +11,28 @@ function Caixa({quantidade,eventoQuan,val,eventoVal}) {
 
     const [restricao, setRestricao] = useState(0)
     const [variaveis, setVariaveis] = useState(0)
+    const [cont,setCont] = useState([])
+    const [atualInput,setAtualInput] = useState();
 
     function variavel(novo) {
-        setVariaveis(novo)
+        setVariaveis(novo.value)
     }
     function restricoes(novo) {
-        setRestricao(novo)
+        setRestricao(novo.value)
     }
+    function matriz (novo){
+       
+        if(atualInput == novo){
+            cont.pop()
+            cont.push(novo.value)
+        }else{
+            
+            // console.log(novo);
+            cont.push(novo.value)
+            setAtualInput(novo)
+        }
+    }
+ 
     function classificar(){
        
         let conjunto = []
@@ -29,16 +44,16 @@ function Caixa({quantidade,eventoQuan,val,eventoVal}) {
               
                 if(i!= val-1){
 
-                    linha.push(<Input name="teste" type="number" /> ,<h1>+</h1>)
+                    linha.push(<Input name="teste" type="number" eventoTexto={matriz} /> ,<h1>+</h1>)
                 }
                 else{
-                    linha.push(<Input name="teste" type="number" />)
+                    linha.push(<Input name="teste" type="number" eventoTexto={matriz} />)
                 }
             }
             if(index == 0){
                 conjunto.push(<div className= {styles.restricao_div}> <Max_min/>{linha}  </div>)
             } 
-            conjunto.push(<div className={styles.restricao_div}>{linha} <Opcoes/> <Input name="teste" type="number" /> </div> )
+            conjunto.push(<div className={styles.restricao_div}>{linha} <Opcoes/> <Input name="teste" type="number"  eventoTexto={matriz} /> </div> )
                 
         }
   
@@ -46,16 +61,21 @@ function Caixa({quantidade,eventoQuan,val,eventoVal}) {
     }
     function testar() {
         
-       
-        eventoQuan(restricao)
-        eventoVal(variaveis)
+        if (quantidade >1) {
+           
+            console.log(cont);
+        }else{
+            eventoQuan(restricao)
+            eventoVal(variaveis)
+        }
+     
      
         
     }
     function unitario(){
         let linha = []
-        linha.push(<Input text="Quantidade de variáveis : " name="quantidade_var" placeholder="Escreva quantas variáveis do problema" type="number" eventoTexto = {variavel}/>)
-        linha.push(<Input text="Quantidade de restrições : " name="quantidade_res" placeholder="Escreva quantas restriçõess do problema" type="number" eventoTexto={restricoes}/>)
+        linha.push(<Input text="Quantidade de variáveis : " name="quantidade_var" placeholder="Escreva quantas variáveis do problema" type="number" eventoTexto = {variavel} idUse="t01"/>)
+        linha.push(<Input text="Quantidade de restrições : " name="quantidade_res" placeholder="Escreva quantas restriçõess do problema" type="number" eventoTexto={restricoes} idUse="t02"/>)
         return linha
     }
     
